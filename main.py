e34d36dba545e857968b8e60f59a6c179887772c
John1571 <johnny96jones@gmail.com>
2023-03-06 18:01:36 -0600

    Communicate with user better.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ water_consumed = 0
 while (time.time() - start_time)/num_sec < time_limit and water_consumed < water_goal:
     if time.time() > last_check + 5:
         try:
-            new_water = int(input("How many ounces do you have now? "))
+            new_water = int(input("\nHow many ounces do you have now? "))
         except ValueError:
             print("invalid value")
             continue
@@ -29,10 +29,11 @@ while (time.time() - start_time)/num_sec < time_limit and water_consumed < water
         print ("You have consumed %i oz of water." % water_consumed)
         time_remaining = time_limit - (last_check - start_time)/60
         print ("You have %i minutes left." % time_remaining)
-        goal_string =  " " if water_consumed > 0 else " not "
+        goal_string =  "YES" if water_consumed > 0 else "NO"
         if (water_consumed and (time_limit*num_sec)/(last_check - start_time) < water_goal/water_consumed):
-            goal_string = " not "
-        print("At this rate you will%smake your goal." % goal_string)
+            goal_string = "NO"
+        print("At this rate will you make your goal?  %s" % goal_string)
+        print("Necessary Rate: %i oz/hr\tYour Rate: %i oz/hr" % (water_goal/(time_limit/60), water_consumed/((time_limit-time_remaining)/60)))
         
 if ((time.time() - start_time)/num_sec >= time_limit):
     print("Time's up.")
